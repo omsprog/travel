1213450c79d31495c3bda686fb169cf8fdbdb1de
omsprog <omsprog@gmail.com>
2024-12-28 01:09:41 -0600

    small example of data layer testin

---
+++ travel-backend/src/test/java/com/omsprog/travel/data/HotelEntityIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.omsprog.travel.data;
+
+import com.omsprog.travel.entity.jpa.HotelEntity;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.math.BigDecimal;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class HotelEntityIntegrationTest {
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void whenCustomerEntity_whenValidCustomerDetailsProvided_shouldReturnStoredCustomerDetails() {
+        // Arrange
+        HotelEntity hotelEntity = HotelEntity
+                .builder()
+                .name("Heaven")
+                .address("Palm Beach, Miami")
+                .rating(5)
+                .price(BigDecimal.valueOf(4000))
+                .build();
+
+        // Act
+        HotelEntity storedHotelEntity = testEntityManager.persistAndFlush(hotelEntity);
+
+        // Assert
+        assertTrue(storedHotelEntity.getId() > 0);
+        assertEquals("Heaven", storedHotelEntity.getName());
+        assertEquals("Palm Beach, Miami", storedHotelEntity.getAddress());
+        assertEquals(5, storedHotelEntity.getRating());
+        assertEquals(BigDecimal.valueOf(4000), storedHotelEntity.getPrice());
+    }
+}
